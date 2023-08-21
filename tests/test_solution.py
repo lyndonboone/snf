6884446e2f568eddb3d65c2e153db99c8eae38a6
Lyndon Boone <lyndonboone8@gmail.com>
2023-08-21 15:26:41 -0400

    Comment out solution tests for now
    Signed-off-by: Lyndon Boone <lyndonboone8@gmail.com>

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -10,40 +10,40 @@
 # limitations under the License.
 
 
-import unittest
+# import unittest
 
-import numpy as np
-from snftools.algo import Solution
-
-
-def dummy_fn(array, c):
-    return array.sum() * c  # no relation to clustering, just wanted to use c
+# import numpy as np
+# from snftools.algo import Solution
 
 
-class TestSolution(unittest.TestCase):
-    def test_len(self):
-        solution = Solution(4, [1, 2, 3, 4])
-        self.assertEqual(len(solution), 4)
-
-    def test_update_fitness(self):
-        solution = Solution(2, [0, 2])
-        data = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-        solution.update_fitness(data, dummy_fn)
-        self.assertEqual(solution.fitness, 32)
-
-    def test_fuse_wrong_c(self):
-        a = Solution(2, [0, 2])
-        b = Solution(3, [3])
-        with self.assertRaises(AssertionError):
-            a.fuse(b)
-
-    def test_fuse(self):
-        a = Solution(2, [0, 2])
-        b = Solution(2, [3])
-        c = a.fuse(b)
-        self.assertEqual(c.c, 2)
-        self.assertListEqual(c.features, [0, 2, 3])
+# def dummy_fn(array, c):
+#     return array.sum() * c  # no relation to clustering, just wanted to use c
 
 
-if __name__ == "__main__":
-    unittest.main()
+# class TestSolution(unittest.TestCase):
+#     def test_len(self):
+#         solution = Solution(4, [1, 2, 3, 4])
+#         self.assertEqual(len(solution), 4)
+
+#     def test_update_fitness(self):
+#         solution = Solution(2, [0, 2])
+#         data = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+#         solution.update_fitness(data, dummy_fn)
+#         self.assertEqual(solution.fitness, 32)
+
+#     def test_fuse_wrong_c(self):
+#         a = Solution(2, [0, 2])
+#         b = Solution(3, [3])
+#         with self.assertRaises(AssertionError):
+#             a.fuse(b)
+
+#     def test_fuse(self):
+#         a = Solution(2, [0, 2])
+#         b = Solution(2, [3])
+#         c = a.fuse(b)
+#         self.assertEqual(c.c, 2)
+#         self.assertListEqual(c.features, [0, 2, 3])
+
+
+# if __name__ == "__main__":
+#     unittest.main()
